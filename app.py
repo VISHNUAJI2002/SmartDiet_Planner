881f6a0d454527dfeccd530735ff1a9477b8fc6f
Vishnu Aji <vishnuaji2002@gmail.com>
2025-10-15 15:11:58 +0530

    Added loopback addresss

--- app.py
+++ app.py
@@ -312,5 +312,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    app.run(host="0.0.0.0", port=port)
